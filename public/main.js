4d3ec2c92e57da33405d168fbc1e6b81cf35e1c0
yellowww <78814395+yellowww@users.noreply.github.com>
2021-12-12 11:35:38 -0500

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -140,7 +140,7 @@ function checkIfCanUpload(callback) {
       var date = new Date();
       var time = date.getTime();
       var timeDif = time-mostRecentUpload;
-      callback(timeDif>=300000);
+      callback(timeDif>=600000);
     };
   });
 }
